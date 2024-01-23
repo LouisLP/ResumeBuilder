f37c4fbeaf916c867a55f5bc63da3ded51e52ef1
LouisLP <louis.lascelles.palys@gmail.com>
2024-01-23 16:57:11 +0100

    Updated connection and routes (app.use)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { database } from "./config/database";
+import resumeRoutes from "./routes/resumeRoutes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose
+	.connect(process.env.MONGO_DB_URI || "mongodb://localhost:27017/jobreel")
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.use(express.json()); // for parsing application/json
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Resume Builder API is running");
@@ -20,6 +30,9 @@ app.get("/database-test", async (req: Request, res: Response) => {
 		res.send("Database connection failed");
 	}
 });
+
+// Use resume routes
+app.use("/api", resumeRoutes);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
