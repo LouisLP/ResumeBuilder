f2382097cef8580af9f88356fd1e33f97ad82c70
LouisLP <louis.lascelles.palys@gmail.com>
2024-01-23 18:41:48 +0100

    Vue router configuration

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,17 +1,34 @@
-// src/router/index.js
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
+import ResumeList from "@/components/ResumeList.vue";
+import ResumeForm from "@/components/ResumeForm.vue";
+import ResumeView from "@/components/ResumeView.vue";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-	// Define your routes here
-];
-
-const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	routes,
+export default new Router({
+	routes: [
+		{
+			path: "/",
+			name: "ResumeList",
+			component: ResumeList,
+		},
+		{
+			path: "/resume/create",
+			name: "CreateResume",
+			component: ResumeForm,
+		},
+		{
+			path: "/resume/edit/:id",
+			name: "EditResume",
+			component: ResumeForm,
+			props: true,
+		},
+		{
+			path: "/resume/:id",
+			name: "ViewResume",
+			component: ResumeView,
+			props: true,
+		},
+	],
 });
-
-export default router;
