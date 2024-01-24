f17f5fde4f3eb6f2ea3655fffc2bfc56be8f36a6
LouisLP <louis.lascelles.palys@gmail.com>
2024-01-24 10:51:27 +0100

    Updated styling for each name

--- client/src/components/ResumeList.vue
+++ client/src/components/ResumeList.vue
@@ -3,11 +3,20 @@
 		<h1>Resumes</h1>
 		<ul>
 			<li class="p-2 list-unstyled" v-for="resume in resumes" :key="resume._id">
-				{{ resume.name }}
-				<router-link class="mx-3 btn btn-primary" :to="{ name: 'ViewResume', params: { id: resume._id } }"
+				<h3>{{ resume.name }}</h3>
+
+				<router-link
+					class="btn btn-primary"
+					:to="{ name: 'ViewResume', params: { id: resume._id } }"
 					>View</router-link
 				>
-				<router-link class="btn btn-secondary" :to="{ name: 'EditResume', params: { id: resume._id } }">Edit</router-link>
+				<router-link
+					class="mx-3 btn btn-secondary"
+					:to="{ name: 'EditResume', params: { id: resume._id } }"
+					>Edit</router-link
+				>
+
+				<button class="btn btn-danger" @click="deleteResume(resume._id)">Delete</button>
 			</li>
 		</ul>
 		<router-link class="btn btn-dark" to="/resume/create">Create New Resume</router-link>
@@ -31,6 +40,19 @@ export default {
 		} catch (error) {
 			console.error("Error fetching resumes:", error);
 		}
+	},
+	methods: {
+		async deleteResume(resumeId) {
+			if (confirm("Are you sure you want to delete this resume?")) {
+				try {
+					await axios.delete(`${process.env.VUE_APP_API_URL}/resumes/${resumeId}`);
+					// Remove the resume from the local array
+					this.resumes = this.resumes.filter((resume) => resume._id !== resumeId);
+				} catch (error) {
+					console.error("Error deleting resume:", error);
+				}
+			}
+		},
 	},
 };
 </script>
